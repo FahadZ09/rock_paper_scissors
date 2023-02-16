8dc386806c634d6238f39cce24be8998d3815982
FAHADZ09 <fahadzaheer9@hotmail.com>
2023-02-16 05:30:08 -0500

    sim 5 games and provide  result in console

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const choices = ["rock", "paper", "scissors"]
 const playerSelection = "rock"
-const computerSelection = getComputerChoice()
-
+let playerScore = 0
+let computerScore = 0
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random()*choices.length)
     const computerChoice = choices[randomChoice]
@@ -15,15 +15,32 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return`You tied! You both picked ${playerSelection}`
     }else if (playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++
         return`You won! Paper beats Rock`
     }else if (playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore++
         return`You won! Rock beats Scissors`
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore++
         return`You won! Scissors beats Paper`
     } else {
+        computerScore++
         return`You lost! ${computerSelection} beats ${playerSelection}`
     }
 }
 
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    for (let i = 0; i < 5 ; i++ ){
+        const computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    }
+    if (playerScore > computerScore){
+        return "You Won! Great Job"
+    } else if (computerScore > playerScore){
+        return "You Lost! Better luck next time"
+    }else {
+        return "You tied! You are a bot"
+    }
+}
+
+    console.log(game())
